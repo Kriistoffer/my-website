6acf1fa9663cf92dde79b65ebf8e7b9641d2add8
Kristoffer <kjohansson93@hotmail.se>
2024-01-21 23:42:08 +0100

    Added dark mode support.

--- components/project.tsx
+++ components/project.tsx
@@ -26,13 +26,13 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[40rem] border border-black/5 overflow-hidden relative sm:pr-8 sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 rounded-lg">
-        <div className="flex flex-col h-full sm:mt-auto pt-4 pb-6 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] group-even:ml-[18rem]">
+      <section className="bg-gray-100 max-w-[40rem] border border-black/5 overflow-hidden relative sm:pr-8 sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 rounded-lg dark:bg-white/10 dark:hover:bg-white/20 dark:text-white">
+        <div className="flex flex-col h-full sm:mt-auto pt-4 pb-6 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-2">
             {tags.map((tag, index) => (
-              <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full" key={index}>
+              <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70" key={index}>
                 {tag}
               </li>
             ))}
@@ -43,7 +43,7 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
           src={imageUrl}
           alt={title}
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition
         
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
