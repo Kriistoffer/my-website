472b4d8816d0b1796a2be1b18b41da595bb2716c
Kristoffer <kjohansson93@hotmail.se>
2024-01-21 21:15:48 +0100

    Experience page which will display previous job(s).

---
+++ components/experience.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React from 'react';
+import Heading from './heading';
+import { useSectionInView } from '@/library/custom-hooks';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { experiencesData } from '@/library/data';
+
+export default function Experience() {
+  const { ref } = useSectionInView('Experience');
+  return (
+    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+      <Heading heading={'My experience'} />
+      {/* TODO: Animations currently broken in this version of Next.js -- look up workarounds or replace with something else. Not priority as everything but the animiations work. */}
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((experience, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              contentStyle={{
+                background: '#f3f4f6',
+                boxShadow: 'none',
+                border: '1px solid rgba(0,0,0,0.05)',
+                textAlign: 'left',
+                padding: '1.3rem 2rem'
+              }}
+              contentArrowStyle={{
+                borderRight: '0.4rem solid #9ca3af'
+              }}
+              date={experience.date}
+              icon={experience.icon}
+              iconStyle={{
+                background: 'white',
+                fontSize: '1.5rem'
+              }}
+              visible={true}
+            >
+              <h3 className="font-semibold capitalize">{experience.title}</h3>
+              <p className="font-normal !mt-0">{experience.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700">{experience.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
+  );
+}
