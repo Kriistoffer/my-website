51e405f5b7a0cbb9dd62678248d2c138d624c904
Kristoffer <kjohansson93@hotmail.se>
2024-01-23 13:32:54 +0100

    Updates to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,10 +22,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`${inter.className} pt-28 text-gray-950 bg-gray-50 relative sm:pt-36 dark:bg-gray-950 dark:text-gray-50 dark:text-opacity-90`}>
-        <div className="bg-[#a03b3e] absolute -z-10 top-[-8rem] right-[-22rem] h-[31.25rem] w-[31.25rem] rounded-full sm:w-[68.75rem] dark:bg-[#4f48af] -rotate-6 blur-[10rem]"></div>
+      <body
+        className={`${inter.className} pt-28 text-gray-950 bg-gray-50 relative sm:pt-36 dark:bg-gray-950 dark:text-gray-50 dark:text-opacity-90 overflow-x-hidden`}
+      >
+        <div className="bg-[#a03b3e] absolute -z-10 top-[-8rem] right-[-22rem] h-[31.25rem] w-[31.25rem] rounded-full sm:w-[36rem] dark:bg-[#4f48af] -rotate-6 blur-[10rem]"></div>
 
-        <div className="bg-[#ff4040] absolute -z-[11] top-[38rem] left-[-40rem] h-[31.25rem] w-[50rem] rounded-full sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-35rem] dark:bg-[#676394] blur-[10rem]"></div>
+        <div className="bg-[#ff4040] absolute -z-10 top-[88rem] rotate-45 blur-[10rem] right-[-22rem] h-[31.25rem] w-[31.25rem] rounded-full sm:w-[36rem] dark:bg-[#4f48af]"></div>
+
+        <div className="bg-[#ff4040] absolute -z-[10] top-[38rem] left-[-44rem] h-[31.25rem] w-[50rem] rounded-full sm:w-[54rem] md:w-[54rem] lg:w-[54rem] xl:w-[58rem] 2xl:w-[68rem] dark:bg-[#676394] blur-[10rem]"></div>
+
+        <div className="bg-[#ff4040] absolute -z-[10] top-[208rem] left-[-44rem] h-[31.25rem] w-[50rem] rounded-full sm:w-[54rem] md:w-[54rem] lg:w-[54rem] xl:w-[58rem] 2xl:w-[68rem] dark:bg-[#4f48af] rotate-90 blur-[10rem]"></div>
+
+        <div className="bg-[#a03b3e] absolute -z-[10] bottom-[-2rem] left-1/2 -translate-x-1/2 h-[6.25rem] w-[10rem] rounded-full sm:w-[54rem] md:w-[54rem] lg:w-[54rem] xl:w-[58rem] 2xl:w-[60rem] dark:bg-[#4f48af] blur-[10rem]"></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
