2f770fac528e6a5f0fd5e3cfba1721ea19d6e7a5
Kristoffer <kjohansson93@hotmail.se>
2024-01-23 02:00:59 +0100

    Playing around with layout design.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`${inter.className} pt-28 bg-gray-50 text-gray-950 relative sm:pt-36 dark:bg-gray-950 dark:text-gray-50 dark:text-opacity-90`}>
-        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+      <body className={`${inter.className} pt-28 text-gray-950 bg-gray-50 relative sm:pt-36 dark:bg-gray-950 dark:text-gray-50 dark:text-opacity-90`}>
+        <div className="bg-[#a03b3e] absolute -z-10 top-[-8rem] right-[-22rem] h-[31.25rem] w-[31.25rem] rounded-full sm:w-[68.75rem] dark:bg-[#4f48af] -rotate-6 blur-[10rem]"></div>
+
+        <div className="bg-[#ff4040] absolute -z-[11] top-[38rem] left-[-40rem] h-[31.25rem] w-[50rem] rounded-full sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-35rem] dark:bg-[#676394] blur-[10rem]"></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
