33b0b60d34980dc55256ae858070b66a3b408d25
Kristoffer <kjohansson93@hotmail.se>
2024-01-23 14:08:26 +0100

    Shuffled the order of the skils + added a border in dark mode.

--- components/skills.tsx
+++ components/skills.tsx
@@ -5,6 +5,7 @@ import Heading from './heading';
 import { skillsData } from '@/library/data';
 import { useSectionInView } from '@/library/custom-hooks';
 import { motion } from 'framer-motion';
+import { shuffleArray } from '@/library/utils';
 
 export default function Skills() {
   const { ref } = useSectionInView('Skills');
@@ -25,10 +26,10 @@ export default function Skills() {
     <section id="skills" ref={ref} className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
       <Heading heading={'My skills'} />
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-        {skillsData.map((skill, index) => (
+        {shuffleArray(skillsData).map((skill, index) => (
           <motion.li
             key={index}
-            className="bg-white blackBorder rounded-full py-3 px-5 dark:bg-white/10 dark:text-white/80"
+            className="bg-white blackBorder rounded-full py-3 px-5 dark:bg-white/10 dark:border dark:border-white/5 dark:text-white/80"
             variants={fadeInAnimation}
             initial={'initial'}
             whileInView={'animate'}
