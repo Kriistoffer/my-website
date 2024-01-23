27e9a0f6dda12bdf610a77a94e9596a91a3e5943
Kristoffer <kjohansson93@hotmail.se>
2024-01-23 14:27:11 +0100

    Removed shuffled order for now.

--- components/skills.tsx
+++ components/skills.tsx
@@ -5,7 +5,6 @@ import Heading from './heading';
 import { skillsData } from '@/library/data';
 import { useSectionInView } from '@/library/custom-hooks';
 import { motion } from 'framer-motion';
-import { shuffleArray } from '@/library/utils';
 
 export default function Skills() {
   const { ref } = useSectionInView('Skills');
@@ -26,7 +25,7 @@ export default function Skills() {
     <section id="skills" ref={ref} className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
       <Heading heading={'My skills'} />
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-        {shuffleArray(skillsData).map((skill, index) => (
+        {skillsData.map((skill, index) => (
           <motion.li
             key={index}
             className="bg-white blackBorder rounded-full py-3 px-5 dark:bg-white/10 dark:border dark:border-white/5 dark:text-white/80"
