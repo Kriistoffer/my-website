fc104878f9fea530b158d88de8bffec0c3c35266
Kristoffer <kjohansson93@hotmail.se>
2024-01-24 16:12:36 +0100

    Language switch -- will remain unused for now.

---
+++ components/language-switch.tsx
@@ -0,0 +1,25 @@
+// 'use client';
+
+// import React from 'react';
+// import '/node_modules/flag-icons/css/flag-icons.min.css';
+// import Link from 'next/link';
+
+// export default function LanguageSwitch() {
+
+//   return (
+//     <button
+//       className="fixed bottom-20 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-black/20 dark:border-white dark:border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 overflow-hidden"
+//       onClick={() => null}
+//     >
+//       {locale === 'en' ? (
+//         <Link href="/" locale={'sv'} scroll={false}>
+//           <span className="fi fi-se text-[3rem] translate-x-1 rounded-full"></span>
+//         </Link>
+//       ) : (
+//         <Link href="/" locale={'en'} scroll={false}>
+//           <span className="fi fi-gb text-[3rem] -translate-x-1 rounded-full"></span>
+//         </Link>
+//       )}
+//     </button>
+//   );
+// }
